36be2cd9389e681aeb338d39258f9889a26e4841
Gustav <gust1224@live.dk>
2020-04-08 11:20:49 +0200

    Initial commit

---
+++ src/main/java/com/grp2055/restbackend/RestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grp2055.restbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grp2055/restbackend/RestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grp2055.restbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
